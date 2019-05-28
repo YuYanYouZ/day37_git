006e35136abef0ee705c963249bb9c3a0071d53f
YuYanYouZ <51064626+YuYanYouZ@users.noreply.github.com>
2019-05-28 11:47:29 +0800

    qw

--- src/com/qianf/test/App.java
+++ src/com/qianf/test/App.java
@@ -4,7 +4,9 @@ public class App {
     public static void main(String[] args) {
 
         System.out.println("张三");
-        //System.out.println("修改");
+        System.out.println("修改");
+        System.out.println("新增");
+        
     }
 
 }
