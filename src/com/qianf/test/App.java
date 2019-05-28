d0b901553bc4615c0edeea97469525d4119adff5
liuG <813812060@qq.com>
2019-05-28 11:48:45 +0800

    change new info

--- src/com/qianf/test/App.java
+++ src/com/qianf/test/App.java
@@ -3,8 +3,11 @@ package com.qianf.test;
 public class App {
     public static void main(String[] args) {
 
-        System.out.println("张三");
+        System.out.println("王二");
         //System.out.println("修改");
+
+
+        int a = 10;
     }
 
 }
